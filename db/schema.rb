59211461f68c841cf3e62a93097a3a2292db9514
Julien Le Mee <julemee@gmail.com>
2022-08-23 14:11:30 -0400

    create a seed and fix a name column in playgrounds table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_162028) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_180547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "playgrounds", force: :cascade do |t|
     t.string "name"
-    t.string "adress"
+    t.string "address"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
