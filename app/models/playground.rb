e70fdcd6545a6d2478c257685c6b19beb269b9ae
Mikael Gonsalves <mjgonsalves@protonmail.com>
2022-08-25 16:38:37 -0400

    images in seeds working and showing in show

--- app/models/playground.rb
+++ app/models/playground.rb
@@ -1,5 +1,5 @@
 class Playground < ApplicationRecord
   has_many :games, dependent: :destroy
-  has_many_attached :photos
+  has_one_attached :photo
   # validates :name, :adress, :description, presence: true
 end
