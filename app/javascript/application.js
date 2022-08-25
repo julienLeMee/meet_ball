3f113d9a4ab088b04c0d431fef3eb5496ed82ea7
Victor Prete <vprete@tuta.io>
2022-08-24 22:53:53 -0400

    Added style and data to new form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,21 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+
+// ./packs/application.js
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+// import Flatpickr
+import Flatpickr from 'stimulus-flatpickr'
+
+// Import style for flatpickr
+require("flatpickr/dist/flatpickr.css")
+
+// Manually register Flatpickr as a stimulus controller
+application.register('flatpickr', Flatpickr)
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,16 @@ class Game < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
   has_many :players
+
+  enum team_size: {
+    '1 v 1': 0,
+    '2 v 2': 1,
+    '3 v 3': 2,
+    '4 v 4': 3
+  }
+
+  enum game_mode: {
+    competitive: 0,
+    casual: 1
+  }
 end
