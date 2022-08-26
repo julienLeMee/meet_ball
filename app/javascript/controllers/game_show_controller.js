e86c1d6030fcef38d54e276a358850b8098dee88
Victor Prete <vprete@tuta.io>
2022-08-26 11:12:58 -0400

    Removed players controller and stimulus game show controller

--- app/javascript/controllers/game_show_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="game-show"
-export default class extends Controller {
-  connect() {
-    console.log("test");
-  }
-}
