3604cb52007598e38aa47af279321c56a6c78099
Julien Le Mee <julemee@gmail.com>
2022-08-28 19:01:14 -0400

    added a chat system

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,38 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  ################# PLAYGROUND #################
+
   resources :playgrounds, only: %i[show index] do
     resources :games, only: %i[new create]
   end
 
-  get '/games/new', to: 'games#new_choose_playground', as: :new_choose_playground
-  post '/games', to: 'games#create_choose_playground', as: :create_choose_playground
-  get '/result/:id/confirmed_results', to: 'results#confirmed_results', as: :confirmed_results
+  ################# GAMES #################
 
   resources :games, only: %i[edit show update destroy] do
     resources :results, only: %i[new create]
   end
 
+  ################# PLAYERS #################
+
   resources :players, only: :create
   patch '/games/:game_id/players', to: 'players#update', as: :update_player
 
+  ################# RESULTS #################
+
   resources :results, only: %i[show]
 
+  ################# CHATROOM #################
+
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
+
+  ################# CUSTOM ROUTES #################
+
+  get '/games/new', to: 'games#new_choose_playground', as: :new_choose_playground
+  post '/games', to: 'games#create_choose_playground', as: :create_choose_playground
+  get '/result/:id/confirmed_results', to: 'results#confirmed_results', as: :confirmed_results
   get '/playgrounds_nearby', to: 'playgrounds#playgrounds_nearby'
   get '/my_games', to: 'games#my_games'
   get '/dashboard', to: 'users#show'
