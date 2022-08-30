fa63e39c3e6a4ca3dc358bb715b87df999543096
Mikael Gonsalves <mjgonsalves@protonmail.com>
2022-08-30 13:51:19 -0400

    the main user has games. And games are scheduled up to sep 3

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,6 +1,6 @@
 class Badge < ApplicationRecord
   has_many :user_badges, dependent: :destroy
-  BADGES = ["faiplay", "teamplayer", "sharpshoter", "mvp", "playmaker", "dribble god"]
+  BADGES = ["faiplay", "rebounder", "sharpshoter", "mvp", "playmaker", "dribble god"]
 
   has_one_attached :photo
 end
