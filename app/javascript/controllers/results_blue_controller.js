099a904d8dde79843404e68ca97051d0e0ed6a31
Victor Prete <vprete@tuta.io>
2022-08-30 16:39:19 -0400

    Added toggle badges div on click and decline results form

--- app/javascript/controllers/results_blue_controller.js
+++ app/javascript/controllers/results_blue_controller.js
@@ -5,9 +5,15 @@ export default class extends Controller {
   static targets = ["badges"];
 
   connect() {
+    window.addEventListener("click", (event) => {
+      if (event.target.contains(this.badgesTarget) && this.badgesTarget.style.display === "block") {
+        this.badgesTarget.style.display = "none";
+      }
+    });
   }
 
   toggleBadges() {
+    this.badgesTarget.style.display = "none";
     this.badgesTarget.style.display = "block";
   }
 }
