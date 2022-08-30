3c174cfcdcd558263be40bae3074d2615ea12a57
Victor Prete <vprete@tuta.io>
2022-08-30 10:03:53 -0400

    Fixed upcoming games

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :players, dependent: :destroy
   has_many :user_badges, dependent: :destroy
   has_many :badges, -> { distinct }, through: :user_badges
-  
+
   validates :username, presence: true, uniqueness: true
   validates :photo, presence: true
 
