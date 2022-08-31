2fd8a965d8cca2d06f5b821d194be3c94fe7f270
Julien Le Mee <julemee@gmail.com>
2022-08-31 16:04:59 -0400

    display see result button without autorization in progress

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,5 +1,5 @@
 class Result < ApplicationRecord
-  belongs_to :game
+  belongs_to :Game
 
   # validates :blue_score, uniqueness: true, numericality: { greater_than_or_equal_to: 0 }
   # validates :red_score, uniqueness: true, numericality: { greater_than_or_equal_to: 0 }
