301a5b1dcdde931e69ba6dd79bb7045b4c122798
Julien Le Mee <julemee@gmail.com>
2022-09-01 13:52:16 -0400

    fixed js issue

--- app/javascript/controllers/game_show_controller.js
+++ app/javascript/controllers/game_show_controller.js
@@ -8,7 +8,7 @@ export default class extends Controller {
     const redTeamFull = (document.querySelector(".team-red").dataset.redFull) === 'true';
     const blueTeamFull = (document.querySelector(".team-blue").dataset.blueFull) === 'true';
 
-    if (redTeamFull) this.redTarget.insertAdjacentHTML('beforeend', '<div class="btn-small team-full">Team Full</div>');
+    if (redTeamFull) this.redTarget.innerHTML = '<div class="btn-small team-full">Team Full</div>';
     if (blueTeamFull) this.blueTarget.innerHTML = '<div class="btn-small team-full">Team Full</div>';
   }
 }
