cd3880ecf273b15614567ea4c7b437a882be1ac4
Victor Prete <vprete@tuta.io>
2022-09-01 16:14:46 -0400

    Added toggle class for create a game form

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,25 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/themes/dark.css';
-
 
 export default class extends Controller {
-  // initialize() {
-  //   console.log("flatpickr")
-  //   this.config = {
-  //     enableTime: true,
-  //     dateFormat: 'Y-m-d',
-  //   }
-
-  // }
   connect() {
     this.initFlatpickr();
-    console.log("connected")
-    console.log(this.element)
   }
 
   initFlatpickr() {
-    console.log("from init")
     const fp = flatpickr('#start_date', {
       minDate: 'today',
       altInput: true,
@@ -29,7 +16,4 @@ export default class extends Controller {
       dateFormat: "Y-m-d H:i"
     });
   }
-
-
-
 }
