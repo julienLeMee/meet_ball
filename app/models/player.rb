303bb7b959ff09970363b727e1bd8f8bea06f07b
Victor Prete <vprete@tuta.io>
2022-08-31 21:10:41 -0400

    Hid rank if game is casual & we can't have two times the same user per game

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,8 @@ class Player < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
+  validates :user, uniqueness: { scope: :game, message: 'Only one player per game' }
+
   enum team: {
     red: 0,
     blue: 1
